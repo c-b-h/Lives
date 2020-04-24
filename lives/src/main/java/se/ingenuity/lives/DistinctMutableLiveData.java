b475e26e5049847f8d558c5c6c088c16d6c94002
Cyrus Bakhtiari-Haftlang <cyrus.bh@gmail.com>
2020-04-24 15:13:20 +0200

    DistinctMutableLiveData

---
+++ lives/src/main/java/se/ingenuity/lives/DistinctMutableLiveData.java
@@ -0,0 +1,47 @@
+package se.ingenuity.lives;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class DistinctMutableLiveData<T> extends LiveData<T> {
+    @NonNull
+    private final Object dataLock = new Object();
+
+    @NonNull
+    private final AtomicBoolean firstTime;
+
+    public DistinctMutableLiveData() {
+        super();
+        firstTime = new AtomicBoolean(true);
+    }
+
+    public DistinctMutableLiveData(T value) {
+        super(value);
+        firstTime = new AtomicBoolean(false);
+    }
+
+    @Override
+    public void setValue(T value) {
+        if (canUpdate(value)) {
+            super.setValue(value);
+        }
+    }
+
+    @Override
+    public void postValue(T value) {
+        synchronized (dataLock) {
+            if (canUpdate(value)) {
+                super.postValue(value);
+            }
+        }
+    }
+
+    private boolean canUpdate(T currentValue) {
+        T previousValue = getValue();
+        return (firstTime.compareAndSet(true, false)
+                || (previousValue == null && currentValue != null)
+                || (previousValue != null && !previousValue.equals(currentValue)));
+    }
+}
